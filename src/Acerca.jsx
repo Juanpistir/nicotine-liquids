22b2ce265244a527a10c06ce1ca1d026e7cc7521
juanpistir <84823227+Juanpistir@users.noreply.github.com>
2023-10-15 06:12:21 -0500

    arreglo mensaje

--- src/Acerca.jsx
+++ src/Acerca.jsx
@@ -27,7 +27,7 @@ function Acerca() {
           <div className="mb-4">
             <label
               htmlFor="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              className="block mb-2 text-sm font-medium text-gray-300"
             >
               Correo electrónico
             </label>
@@ -36,7 +36,7 @@ function Acerca() {
               type="email"
               name="email"
               placeholder="Tu dirección de correo"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              className="shadow-sm border border-gray-300 text-lg rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 bg-gray-700 placeholder-gray-400 text-white focus:ring-primary-500 border-primary-500 shadow-sm-light"
             />
             <ValidationError
               prefix="Email"
@@ -47,7 +47,7 @@ function Acerca() {
           <div className="sm:col-span-2">
             <label
               htmlFor="message"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              className="block mb-2 text-sm font-medium text-gray-300"
             >
               Mensaje:
             </label>
@@ -56,7 +56,7 @@ function Acerca() {
               name="message"
               placeholder="Escribe aquí tu mensaje"
               rows="6"
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              className="shadow-sm border border-gray-300 block p-2.5 w-full text-lg rounded-lg focus:ring-primary-500 focus:border-primary-500 bg-gray-700 placeholder-gray-400 text-white focus:ring-primary-500 border-primary-500 shadow-sm-light"
             />
             <ValidationError
               prefix="Message"
