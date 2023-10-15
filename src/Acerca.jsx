19c3d442c8fa753fef0370bd93948fd8b42804af
juanpistir <84823227+Juanpistir@users.noreply.github.com>
2023-10-15 06:03:32 -0500

    letra

--- src/Acerca.jsx
+++ src/Acerca.jsx
@@ -14,7 +14,7 @@ function Acerca() {
   return (
     <section className="bg-gray-900 min-h-screen rounded-lg p-4 mx-2">
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-blue-500">
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-blue-500">
           Contáctame
         </h2>
         <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
